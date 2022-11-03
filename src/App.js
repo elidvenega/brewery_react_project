bbd64f6392fbb66b78c692f08f6e04ab07644ce6
elidvenega <venegaelid@gmail.com>
2022-11-03 13:40:58 -0700

    Worked on project

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import './App.css';
-
 import Home from './pages/Home';
+import './scss/style.scss';
 
 export default function App() {
   return (
--- src/components/Navbar.component.jsx
+++ src/components/Navbar.component.jsx
@@ -3,8 +3,19 @@ import Image from '../assets/logo.webp';
 
 export const Navbar = () => {
   return (
-    <>
+    <div className="logo">
      <img src={Image} alt="logo" />
-    </>
+     <button>Order Now</button>
+
+     <ul className="nav_links">
+      <li>HOME</li>
+      <li>ORDER ONLINE</li>
+      <li>GIFT CARD</li>
+      <li>ALE AND ELIXIR</li>
+      <li>CONTACT US</li>
+      <li>LORE</li>
+      <li>COOKBOOK</li>
+     </ul>
+    </div>
   )
 }
